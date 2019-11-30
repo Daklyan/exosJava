51c65bee6c0d8898a6a84f6b04d8b152b423387b
Daklyan <daklyan@protonmail.com>
2019-11-30 20:07:14 +0100

    Sample ex

---
+++ labs/one/parta/Example.java
@@ -0,0 +1,10 @@
+package labs.one.parta;
+
+public class Example{
+	public static void main(String[] args){
+		int i;
+		for(i = 0; i < args.length; ++i){
+			System.out.println(i+1 + " : " + args[i]);
+		}
+	}
+}
